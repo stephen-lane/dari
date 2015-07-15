b0e19d8a2f1bafd9e879eb0b65729c8b2e3b469b
Robert Seeger <rseeger@perfectsensedigital.com>
2015-07-15 10:36:23 -0400

    placeholder classes for field type tests

---
+++ db/src/test/java/com/psddev/dari/db/SqlDatabase_FieldTypes_String_Test.java
@@ -0,0 +1,42 @@
+package com.psddev.dari.db;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by rhseeger on 7/8/15.
+ */
+public class SqlDatabase_FieldTypes_String_Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SqlDatabase_FieldTypes_String_Test.class);
+
+    @ClassRule
+    public static final SqlDatabaseRule res = new SqlDatabaseRule();
+    @Rule
+    public TestName name = new TestName();
+
+
+    @BeforeClass
+    public static void beforeClass() {}
+
+    @AfterClass
+    public static void afterClass() {}
+
+    @Before
+    public void before() {
+        LOGGER.info("Running test [{}]", name.getMethodName());
+    }
+
+    @After
+    public void after() {}
+
+
+    // TODO: everything
+    
+}
