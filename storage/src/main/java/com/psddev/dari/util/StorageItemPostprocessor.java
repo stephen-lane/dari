76c26d9214add9fc02fcc8b3839ce317271c60ba
Ryan-Anderson <ryan@perfectsensedigital.com>
2015-10-08 15:46:37 -0400

    Adds javadoc for StorageItemPostprocessor and StorageItemPreprocessor

--- storage/src/main/java/com/psddev/dari/util/StorageItemPostprocessor.java
+++ storage/src/main/java/com/psddev/dari/util/StorageItemPostprocessor.java
@@ -1,5 +1,19 @@
 package com.psddev.dari.util;
 
+import java.io.InputStream;
+
+/**
+ * StorageItemPostprocessors are executed after an AbstractStorageItem
+ * has executed {@link AbstractStorageItem#saveData(InputStream)} if
+ * the AbstractStorageItem was created by {@link StorageItemFilter}.
+ */
 public interface StorageItemPostprocessor {
+
+    /**
+     * Invoked from StorageItemFilter to handle additional
+     * processing of a StorageItem.
+     *
+     * @param item StorageItem that has been saved to storage.
+     */
     void process(StorageItem item);
 }
--- storage/src/main/java/com/psddev/dari/util/StorageItemPreprocessor.java
+++ storage/src/main/java/com/psddev/dari/util/StorageItemPreprocessor.java
@@ -1,8 +1,20 @@
 package com.psddev.dari.util;
 
+import java.io.InputStream;
+
 import org.apache.commons.fileupload.FileItem;
 
+/**
+ * StorageItemPreprocessors are executed before a StorageItem
+ * has executed {@link StorageItem#save()} in {@link StorageItemFilter}.
+ */
 public interface StorageItemPreprocessor {
 
+    /**
+     * Invoked by StorageItemListener
+     *
+     * @param storageItem StorageItem to be processed
+     * @param fileItem FileItem from the multipart request
+     */
     void process(StorageItem storageItem, FileItem fileItem);
 }
