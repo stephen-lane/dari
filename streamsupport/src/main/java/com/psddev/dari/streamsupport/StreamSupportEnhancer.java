4bc05c6805353fe0e031496d16f5efbded811983
Andrew Miller <andrew@perfectsensedigital.com>
2016-01-19 09:46:31 -0500

    Java7 - Initial automatic conversion to StreamSupport.

---
+++ streamsupport/src/main/java/com/psddev/dari/streamsupport/StreamSupportEnhancer.java
@@ -0,0 +1,181 @@
+package com.psddev.dari.streamsupport;
+
+import com.psddev.dari.util.ClassEnhancer;
+import com.psddev.dari.util.StringUtils;
+import com.psddev.dari.util.asm.AnnotationVisitor;
+import com.psddev.dari.util.asm.ClassReader;
+import com.psddev.dari.util.asm.FieldVisitor;
+import com.psddev.dari.util.asm.Handle;
+import com.psddev.dari.util.asm.Label;
+import com.psddev.dari.util.asm.MethodVisitor;
+import com.psddev.dari.util.asm.Opcodes;
+import com.psddev.dari.util.asm.TypePath;
+
+public class StreamSupportEnhancer extends ClassEnhancer {
+
+    @Override
+    public boolean canEnhance(ClassReader reader) {
+        return true;
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        if (interfaces != null) {
+            String[] java7Interfaces = new String[interfaces.length];
+            for (int j = 0; j < interfaces.length; j++) {
+                java7Interfaces[j] = convertJava8StreamToStreamSupport(interfaces[j]);
+            }
+            interfaces = java7Interfaces;
+        }
+
+        signature = convertJava8StreamToStreamSupport(signature);
+
+        super.visit(version, access, name, signature, superName, interfaces);
+    }
+
+    @Override
+    public void visitSource(String source, String debug) {
+        source = convertJava8StreamToStreamSupport(source);
+        debug = convertJava8StreamToStreamSupport(debug);
+        super.visitSource(source, debug);
+    }
+
+    @Override
+    public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
+
+        desc = convertJava8StreamToStreamSupport(desc);
+        signature = convertJava8StreamToStreamSupport(signature);
+
+        return super.visitField(access, name, desc, signature, value);
+    }
+
+    @Override
+    public void visitOuterClass(String owner, String name, String desc) {
+        owner = convertJava8StreamToStreamSupport(owner);
+        desc = convertJava8StreamToStreamSupport(desc);
+        super.visitOuterClass(owner, name, desc);
+    }
+
+    @Override
+    public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
+        desc = convertJava8StreamToStreamSupport(desc);
+        return super.visitAnnotation(desc, visible);
+    }
+
+    @Override
+    public AnnotationVisitor visitTypeAnnotation(int typeRef, TypePath typePath, String desc, boolean visible) {
+        desc = convertJava8StreamToStreamSupport(desc);
+        return super.visitTypeAnnotation(typeRef, typePath, desc, visible);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(
+            int access,
+            String name,
+            String desc,
+            String signature,
+            String[] exceptions) {
+
+        desc = convertJava8StreamToStreamSupport(desc);
+        signature = convertJava8StreamToStreamSupport(signature);
+
+        MethodVisitor visitor = super.visitMethod(access, name, desc, signature, exceptions);
+
+        System.out.println("MethodVisitor " + name + " ||| " + desc + " ||| " + signature);
+
+        return new MethodVisitor(Opcodes.ASM5, visitor) {
+            @Override
+            public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
+                desc = convertJava8StreamToStreamSupport(desc);
+                return super.visitAnnotation(desc, visible);
+            }
+
+            @Override
+            public AnnotationVisitor visitInsnAnnotation(int typeRef, TypePath typePath, String desc, boolean visible) {
+                desc = convertJava8StreamToStreamSupport(desc);
+                return super.visitInsnAnnotation(typeRef, typePath, desc, visible);
+            }
+
+            @Override
+            public void visitInvokeDynamicInsn(String name, String desc, Handle bsm, Object... bsmArgs) {
+                desc = convertJava8StreamToStreamSupport(desc);
+                super.visitInvokeDynamicInsn(name, desc, bsm, bsmArgs);
+            }
+
+            @Override
+            public void visitLocalVariable(String name, String desc, String signature, Label start, Label end, int index) {
+                desc = convertJava8StreamToStreamSupport(desc);
+                signature = convertJava8StreamToStreamSupport(signature);
+                super.visitLocalVariable(name, desc, signature, start, end, index);
+            }
+
+            @Override
+            public AnnotationVisitor visitLocalVariableAnnotation(int typeRef, TypePath typePath, Label[] start, Label[] end, int[] index, String desc, boolean visible) {
+                desc = convertJava8StreamToStreamSupport(desc);
+                return super.visitLocalVariableAnnotation(typeRef, typePath, start, end, index, desc, visible);
+            }
+
+            @Override
+            public void visitMultiANewArrayInsn(String desc, int dims) {
+                desc = convertJava8StreamToStreamSupport(desc);
+                super.visitMultiANewArrayInsn(desc, dims);
+            }
+
+            @Override
+            public AnnotationVisitor visitTryCatchAnnotation(int typeRef, TypePath typePath, String desc, boolean visible) {
+                desc = convertJava8StreamToStreamSupport(desc);
+                return super.visitTryCatchAnnotation(typeRef, typePath, desc, visible);
+            }
+
+            @Override
+            public AnnotationVisitor visitTypeAnnotation(int typeRef, TypePath typePath, String desc, boolean visible) {
+                desc = convertJava8StreamToStreamSupport(desc);
+                return super.visitTypeAnnotation(typeRef, typePath, desc, visible);
+            }
+
+            @Override
+            public void visitFieldInsn(int opcode, String owner, String name, String desc) {
+                owner = convertJava8StreamToStreamSupport(owner);
+                desc = convertJava8StreamToStreamSupport(desc);
+                super.visitFieldInsn(opcode, owner, name, desc);
+            }
+
+            @Override
+            public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
+
+                if (owner.equals("java/util/Collection") && name.equals("stream")) {
+                    System.out.println("Converting collection.stream");
+                    opcode = 184;
+                    owner = "java8/util/stream/StreamSupport";
+                    desc = "(Ljava/util/Collection;)Ljava8/util/stream/Stream;";
+                    itf = false;
+                }
+
+                owner = convertJava8StreamToStreamSupport(owner);
+                desc = convertJava8StreamToStreamSupport(desc);
+
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
+
+            }
+        };
+    }
+
+    private String convertJava8StreamToStreamSupport(String input) {
+        if (!StringUtils.isBlank(input)) {
+            if (input.contains("java/util/function/")) {
+                input = input.replace("java/util/function/", "java8/util/function/");
+            }
+
+            if (input.contains("java/util/stream/")) {
+                input = input.replace("java/util/stream/", "java8/util/stream/");
+            }
+
+            if (input.contains("java/util/Optional")) {
+                input = input.replace("java/util/Optional", "java8/util/Optional");
+            }
+        }
+
+        return input;
+    }
+
+}
