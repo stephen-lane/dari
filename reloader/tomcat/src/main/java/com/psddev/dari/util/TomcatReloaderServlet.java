aab24f690697574daa2c600f9c4741f2f18b1738
Hyoo Lim <hyoolim@gmail.com>
2016-06-28 12:56:32 -0400

    Formatting.

--- reloader/tomcat/src/main/java/com/psddev/dari/util/TomcatReloaderServlet.java
+++ reloader/tomcat/src/main/java/com/psddev/dari/util/TomcatReloaderServlet.java
@@ -24,7 +24,7 @@ public class TomcatReloaderServlet extends HttpServlet implements ContainerServl
 
     private Wrapper wrapper;
     private Host host;
-    private final AtomicReference<CountDownLatch> latchReference = new AtomicReference<CountDownLatch>();
+    private final AtomicReference<CountDownLatch> latchReference = new AtomicReference<>();
 
     @Override
     public Wrapper getWrapper() {
@@ -36,19 +36,21 @@ public class TomcatReloaderServlet extends HttpServlet implements ContainerServl
         if (wrapper == null) {
             this.wrapper = null;
             this.host = null;
+
         } else {
             this.wrapper = wrapper;
-            this.host = (Host) ((Context) wrapper.getParent()).getParent();
+            this.host = (Host) wrapper.getParent().getParent();
         }
     }
 
     @Override
     protected void service(
-            final HttpServletRequest request,
-            final HttpServletResponse response)
+            HttpServletRequest request,
+            HttpServletResponse response)
             throws IOException, ServletException {
 
         String action = request.getParameter(SourceFilter.RELOADER_ACTION_PARAMETER);
+
         if (SourceFilter.RELOADER_PING_ACTION.equals(action)) {
             response.setContentType("text/plain");
             response.setCharacterEncoding("UTF-8");
@@ -73,62 +75,82 @@ public class TomcatReloaderServlet extends HttpServlet implements ContainerServl
         // If context is still loading, display a message and wait.
         response.setContentType("text/html");
         response.setCharacterEncoding("UTF-8");
-        new HtmlWriter(response.getWriter()) { {
 
+        new HtmlWriter(response.getWriter()) { {
             putDefault(StackTraceElement.class, HtmlFormatter.STACK_TRACE_ELEMENT);
             putDefault(Throwable.class, HtmlFormatter.THROWABLE);
 
             writeTag("!doctype html");
             writeStart("html");
+            {
                 writeStart("head");
+                {
 
                     writeStart("title").writeHtml("Reloader").writeEnd();
 
-                    writeStart("link",
+                    writeElement("link",
                             "href", JspUtils.getAbsolutePath(request, "/_resource/bootstrap/css/bootstrap.css"),
                             "rel", "stylesheet",
                             "type", "text/css");
+
                     writeStart("style", "type", "text/css");
+                    {
                         write(".hero-unit { background: transparent; left: 0; margin: -72px 0 0 60px; padding: 0; position: absolute; top: 50%; }");
                         write(".hero-unit h1 { line-height: 1.33; }");
+                    }
                     writeEnd();
-
+                }
                 writeEnd();
+
                 writeStart("body");
+                {
 
                     writeStart("div", "class", "hero-unit");
+                    {
                         writeStart("h1");
+                        {
                             writeHtml("Reloading ");
                             writeHtml(contextPath);
                             writeHtml("/");
+                        }
                         writeEnd();
+
                         try {
                             writeStart("ul", "class", "muted");
+
                             try {
                                 writeStart("li");
-                                    writeHtml("Waiting for it to start back up");
+                                writeHtml("Waiting for it to start back up");
                                 writeEnd();
-                                for (int i = 0; i < 4000; ++ i) {
+
+                                for (int i = 0; i < 4000; ++i) {
                                     writeHtml(" ");
                                 }
                                 writeHtml("\r\n");
                                 flush();
+
                                 reload(request, response, contextPath, requestPath);
+
                                 writeStart("li");
-                                    writeHtml("Ready!");
+                                writeHtml("Ready!");
                                 writeEnd();
+
                                 writeStart("script", "type", "text/javascript");
-                                    write("location.href = '" + StringUtils.escapeJavaScript(requestPath) + "';");
+                                write("location.href = '" + StringUtils.escapeJavaScript(requestPath) + "';");
                                 writeEnd();
+
                             } finally {
                                 writeEnd();
                             }
+
                         } catch (Exception ex) {
                             writeObject(ex);
                         }
+                    }
                     writeEnd();
-
+                }
                 writeEnd();
+            }
             writeEnd();
         } };
     }
@@ -145,12 +167,15 @@ public class TomcatReloaderServlet extends HttpServlet implements ContainerServl
         if (latchReference.compareAndSet(null, latch)) {
             try {
                 Context context = (Context) host.findChild(contextPath);
+
                 if (context != null) {
                     context.reload();
+
                 } else {
                     throw new IllegalArgumentException(String.format(
                             "No context matching [%s]!", contextPath));
                 }
+
             } finally {
                 latch.countDown();
                 latchReference.set(null);
@@ -158,28 +183,34 @@ public class TomcatReloaderServlet extends HttpServlet implements ContainerServl
 
         } else {
             latch = latchReference.get();
+
             if (latch != null) {
                 try {
                     latch.await();
+
                 } catch (InterruptedException ex) {
                     // Ignore thread interruption and continue.
                 }
             }
         }
 
-        List<Throwable> errors = new ArrayList<Throwable>();
+        List<Throwable> errors = new ArrayList<>();
+
         for (int i = 0; i < 20; ++ i) {
             try {
                 URL pingUrl = new URL(JspUtils.getHostUrl(request) + contextPath + SourceFilter.Static.getInterceptPingPath());
+
                 if ("OK".equals(IoUtils.toString(pingUrl))) {
                     return;
                 }
+
             } catch (IOException ex) {
                 errors.add(ex);
             }
 
             try {
                 Thread.sleep(2000);
+
             } catch (InterruptedException ex) {
                 break;
             }
