83c78a1cab678f2464f2f63b820f3e6cbcacf062
Lee Teague <lee@perfectsensedigital.com>
2016-07-15 12:23:38 -0400

    Add taskHost setting to all RepeatingTasks

--- util/src/main/java/com/psddev/dari/util/RepeatingTask.java
+++ util/src/main/java/com/psddev/dari/util/RepeatingTask.java
@@ -1,12 +1,22 @@
 package com.psddev.dari.util;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeFieldType;
 
-/** Task that repeatedly runs at a predictable interval. */
+/**
+ * Task that repeatedly runs at a predictable interval.
+ *
+ * Optionally specify the single host on which a task should execute with the
+ * setting "dari/taskHost/{class name}". If a task host is not specified, the
+ * the task runs on all hosts.
+ */
 public abstract class RepeatingTask extends Task {
+
+    public static final String TASK_HOST_SETTING_PREFIX = "dari/taskHost/";
 
     private final AtomicReference<DateTime> previousRunTime = new AtomicReference<DateTime>(calculateRunTime(new DateTime()));
 
@@ -45,6 +55,11 @@ public abstract class RepeatingTask extends Task {
         DateTime now = new DateTime();
         DateTime oldPrevious = previousRunTime.get();
         DateTime newPrevious = calculateRunTime(now);
+        String taskHost = Settings.get(String.class, TASK_HOST_SETTING_PREFIX + getClass().getName());
+
+        if (!isTaskHost(taskHost)) {
+            return;
+        }
 
         if (!now.isBefore(newPrevious)
                 && oldPrevious.isBefore(newPrevious)
@@ -52,6 +67,21 @@ public abstract class RepeatingTask extends Task {
             doRepeatingTask(newPrevious);
         } else {
             skipRunCount();
+        }
+    }
+
+    private static boolean isTaskHost(String hostname) {
+        if (hostname == null || "localhost".equals(hostname)) {
+            return true;
+        }
+
+        try {
+            InetAddress allowed = InetAddress.getByName(hostname);
+            InetAddress local = InetAddress.getLocalHost();
+            return local.getHostAddress().equals(allowed.getHostAddress());
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException("Unknown host exception during task execution", e);
         }
     }
 }
